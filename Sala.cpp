2cf5e76957514b099ecb5728b19f670e1456375b
linnce3 <steven102000@hotmail.com>
2024-09-16 14:56:33 -0500

    Update Sala.cpp

--- Sala.cpp
+++ Sala.cpp
@@ -1,11 +1,25 @@
 #include "Sala.h"
+#include <iostream>
+#include <string>
 
-Sala::Sala()
+using namespace std;
+
+Sala::Sala() {}  // Constructor por defecto
+Sala::~Sala() {} // Destructor
+
+void Sala::Programacion(Pelicula _pelicula, int _hora_inicio, int _minuto_inicio, int _hora_fin, int _minuto_fin)
+// Establecer el horario de la película
 {
-    //ctor
+    hora_inicio = _hora_inicio;
+    minuto_inicio = _minuto_inicio;
+    hora_fin = _hora_fin;
+    minuto_fin = _minuto_fin;
 }
 
-Sala::~Sala()
+void Sala::getProgramacion(Pelicula _pelicula)
+// Mostrar la programación de la película
 {
-    //dtor
+    cout << "Programación de la película '" << _pelicula.getNombre() << "':" << endl;
+    cout << "Hora de inicio: " << hora_inicio << ":" << minuto_inicio << endl;
+    cout << "Hora de fin: " << hora_fin << ":" << minuto_fin << endl;
 }
